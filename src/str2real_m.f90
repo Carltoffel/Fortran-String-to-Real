fed2b5e9eaa481fb32ebf792cff5fc1d4feb39e0
Carl Burkert <c.burkert@fz-juelich.de>
2021-11-05 13:58:23 +0100

    Rename variables and constants

--- src/str2real_m.f90
+++ src/str2real_m.f90
@@ -5,26 +5,33 @@ public :: str2real
 
 integer, parameter :: wp    = kind(1.0d0)
 integer, parameter :: ikind = selected_int_kind(2)
-integer(kind=ikind), parameter :: period     = -2
-integer(kind=ikind), parameter :: char_e_cap = 21
-integer(kind=ikind), parameter :: char_e     = char_e_cap + 32
-integer(kind=ikind), parameter :: minus_sign = -3
-real(wp),  parameter :: expo(*) = &
+integer(kind=ikind), parameter :: digit_0    = ichar('0')
+integer(kind=ikind), parameter :: period     = ichar('.') - digit_0
+integer(kind=ikind), parameter :: minus_sign = ichar('-') - digit_0
+
+real(wp), parameter :: whole_number_base(*) =                    &
         [1d15,  1d14,  1d13,  1d12,  1d11,  1d10,  1d9,   1d8,   &
-         1d7,   1d6,   1d5,   1d4,   1d3,   1d2,   1d1,   1d0,   &
-         0d0,                                                    &
-         1d-1,  1d-2,  1d-3,  1d-4,  1d-5,  1d-6,  1d-7,  1d-8,  &
+         1d7,   1d6,   1d5,   1d4,   1d3,   1d2,   1d1,   1d0]
+real(wp), parameter :: fractional_base(*)   =                    &
+        [1d-1,  1d-2,  1d-3,  1d-4,  1d-5,  1d-6,  1d-7,  1d-8,  &
          1d-9,  1d-10, 1d-11, 1d-12, 1d-13, 1d-14, 1d-15, 1d-16, &
          1d-17, 1d-18, 1d-19, 1d-20, 1d-21, 1d-22, 1d-23, 1d-24]
+real(wp), parameter :: period_skip = 0d0
+real(wp), parameter :: base(*) = &
+        [whole_number_base, period_skip, fractional_base]
 
 contains
 
 
 
 function str2real(s) result(r)
-character(*), target, intent(in) :: s
+character(*), intent(in) :: s
 real(wp) :: r
-integer,   parameter :: N = 32
+
+real(wp) :: r_coefficient
+real(wp) :: r_exponent
+
+integer, parameter :: N = 32
 character(N) :: equ_s
 integer(kind=ikind)    :: equ_i(N)
 integer(kind=ikind)    :: mask(N)
@@ -32,14 +39,12 @@ integer      :: period_loc
 integer      :: exponent_loc
 integer      :: mask_from
 integer      :: mask_till
-real(wp)     :: r_base
-real(wp)     :: r_expo
 
 
 equivalence(equ_i, equ_s)
 
 equ_s = s
-equ_i = equ_i - 48
+equ_i = equ_i - digit_0
 
 period_loc   = findloc(equ_i, period, 1)
 !exponent_loc = max( &
@@ -58,17 +63,17 @@ end where
 mask_from = 18 - period_loc
 mask_till = mask_from + exponent_loc - 2
 
-r_base = sum( &
+r_coefficient = sum( &
         equ_i(:exponent_loc - 1)  * &
-        expo(mask_from:mask_till) * &
+        base(mask_from:mask_till) * &
         mask(:exponent_loc - 1))
-r_expo = sum( &
+r_exponent = sum( &
         equ_i(exponent_loc+1:len(s)) * &
         mask(exponent_loc+1:len(s))  * &
-        expo(17-(len(s)-exponent_loc):16))
-if(equ_i(exponent_loc+1) == minus_sign) r_expo = -r_expo
-if(equ_i(1)              == minus_sign) r_base = -r_base
-r = r_base * 10 ** r_expo
+        base(17-(len(s)-exponent_loc):16))
+if(equ_i(exponent_loc+1) == minus_sign) r_exponent = -r_exponent
+if(equ_i(1)              == minus_sign) r_coefficient = -r_coefficient
+r = r_coefficient * 10 ** r_exponent
 end function str2real
 
 
